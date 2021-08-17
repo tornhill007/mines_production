58a603964914720528dec9962cabc65540c2af18
tornhill007 <tornhill1997@gmail.com>
2021-08-17 17:44:36 +0300

    deploying version 3.0

--- config/database.js
+++ config/database.js
@@ -1,24 +1,24 @@
 const {Sequelize} = require('sequelize');
 require('dotenv').config();
 
-module.exports = new Sequelize('mines', 'andrewkomar', '12345', {
-  host: 'localhost',
-  dialect: 'postgres',
-  define: {
-    timestamps: false
-  },
-});
-
-// module.exports = new Sequelize(process.env.DATABASE_URL, {
+// module.exports = new Sequelize('mines', 'andrewkomar', '12345', {
+//   host: 'localhost',
 //   dialect: 'postgres',
-//   protocol: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   },
 //   define: {
 //     timestamps: false
 //   },
 // });
+
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  define: {
+    timestamps: false
+  },
+});
